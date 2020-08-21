36b16b1952d1f4023b9ec95da67cf768dd6ebec7
Constantin <constantin.b@gmail.com>
2020-08-21 10:25:46 +0300

    Check if file exists

--- index.php
+++ index.php
@@ -183,7 +183,7 @@ textarea.scrollTop = textarea.scrollHeight;
 		
 		$error_log = plugin_dir_path( __FILE__ ) . 'error_log';
 		//*
-		if( filesize( $error_log ) >= pow(1024, 2) ){
+		if( file_exists( $error_log ) && filesize( $error_log ) >= pow(1024, 2) ){
 			$filename = wp_unique_filename( plugin_dir_path( __FILE__ ), 'error_log' );
 			$result = rename( $error_log , plugin_dir_path( __FILE__ ) . $filename );
 			if( !$result ){
